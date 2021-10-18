896c97fba4008b11f36ad405332d47cdfa66d36c
ginkoemdon <emdo0001@algonquinlive.com>
2021-10-18 14:53:13 -0400

    File Setup

---
+++ script.js
@@ -0,0 +1,3 @@
+//all new js goes in this file!
+
+//ASSIGNMENT LINK: https://brightspace.algonquincollege.com/d2l/lms/dropbox/user/folder_submit_files.d2l?db=345595&grpid=0&isprv=0&bp=0&ou=370685
--- stories.js
+++ stories.js
@@ -24,6 +24,12 @@ const stories = [
       'Noun',
       'Container'
     ],
+    //goals is to have printed madlib story printed in the console with words subsitutes in
+
+    //output method resturns a template literal
+    //template literal contains a 
+    //need to conrsuct an object of keys (from words array) and values (whatever the user typed into the form) 
+    //subsituting the value with whatever words paremter values 
     output: function (words) {
       return `<p>Make sure your lunch <span class="word">${words.Container}</span> is filled with <span class="word">${words['Adjective 1']}</span> food. Do not go to the <span class="word">${words['Adjective 2']}</span> food stand across the street from the school. The hamburgers they serve are fried in <span class="word">${words.Noun}</span> and are made of <span class="word">${words.Animal}</span> meat. So take a sandwich made of <span class="word">${words['Vegetable 1']}</span> or <span class="word">${words['Vegetable 2']}</span>, it's much healthier!</p>`
     }
